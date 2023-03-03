90f85a2b859fd3274c97cdb97d04b08deb239707
turismo <118571225+moico0996@users.noreply.github.com>
2023-03-03 10:38:08 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+function ingresar(){
+    let name = document.getElementById("name").value;
+    let txt;
+
+
+    if (name==null ||name==0 || name.length<=1|| name.length>=15  ) {
+      txt = "texto invalido";
+    } else {
+      txt = "valido";
+    }
+    document.getElementById("dimo").style.color="red";
+    document.getElementById("dimo").innerHTML = txt;
+
+
+
+    let id = document.getElementById("id").value;
+    let ttt;
+
+
+    if (id==null ||id==0 || id.length<=1|| id.length>=30  ) {
+      ttt = "correo no valido";
+    } else {
+      ttt = "valido";
+    }   
+    document.getElementById("domo").style.color="red";
+    document.getElementById("domo").innerHTML = ttt;
+
+
+
+    let comments=document.getElementById("comments").value; 
+    let tot;
+
+    if(comments===null||id==0 || id.length<=1|| id.length>=20 ){
+        tot="debes mejorar tu comentario";
+    } else{
+        tot="gracias por tu comentario";
+    }
+    document.getElementById("com").style.color="red";
+    document.getElementById("com").innerHTML=tot;
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
